c7f76660ad1c726bd493f2b8de44a2af9bb665fa
Omar <oa10003000@gmail.com>
2024-01-22 15:47:23 +0200

    Prepare for deployment

--- src/pages/not-found/404.jsx
+++ src/pages/not-found/404.jsx
@@ -1,19 +1,51 @@
-import { useTheme } from "@emotion/react";
-import { Box, Typography } from "@mui/material";
 import React from "react";
+import { Box, Button, Container, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import { Link } from "react-router-dom";
 
-const NotFound = () => {
-  const theme = useTheme();
+export default function NotFoound() {
   return (
-    <Box>
-      <Typography color={theme.palette.error.main} variant="h5">
-        There is no design yet
-      </Typography>
-      <br />
-      <br />
-      Please try later...
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Container maxWidth="md">
+        <Grid container spacing={2}>
+          <Grid xs={6}>
+            <Typography variant="h1">404</Typography>
+            <Typography variant="h6">
+              The page you’re looking for doesn’t exist.
+            </Typography>
+            <Link
+              to="/"
+              style={{
+                display: "inline-block",
+                padding: "10px",
+                marginTop: "10px",
+                background: "#8357bd",
+                textDecoration: "none",
+                color: "white",
+                borderRadius: "5px",
+              }}
+              variant="contained"
+            >
+              Back Home
+            </Link>
+          </Grid>
+          <Grid xs={6}>
+            <img
+              src="https://cdn.pixabay.com/photo/2017/03/09/12/31/error-2129569__340.jpg"
+              alt=""
+              width={500}
+              height={250}
+            />
+          </Grid>
+        </Grid>
+      </Container>
     </Box>
   );
-};
-
-export default NotFound;
+}
